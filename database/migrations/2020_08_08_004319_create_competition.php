ff15cfb3668bcf081fc3d2dd19e14e7eb96c4a3d
Rafael Keller <faelkeller@gmail.com>
2020-08-07 22:23:14 -0300

    Criando a tabela de competições

---
+++ database/migrations/2020_08_08_004319_create_competition.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompetition extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('competition', function (Blueprint $table) {
+            $table->smallIncrements("id");
+            $table->string("name", 70);
+            $table->string("slug", 10);
+            $table->text("description")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('competition');
+    }
+}
