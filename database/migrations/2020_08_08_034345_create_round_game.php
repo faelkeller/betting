abf3bc47557984965c6e800a09e78af70c5693d6
Rafael Keller <faelkeller@gmail.com>
2020-08-08 01:11:03 -0300

    Criando a tabela de ligação de rodadas e jogos

---
+++ database/migrations/2020_08_08_034345_create_round_game.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoundGame extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('round_game', function (Blueprint $table) {
+            $table->unsignedMediumInteger('round_id');
+            $table->unsignedMediumInteger('game_id');
+            $table->timestamps();
+            $table->foreign('round_id')->references('id')->on('rounds');
+            $table->foreign('game_id')->references('id')->on('games');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('round_game');
+    }
+}
